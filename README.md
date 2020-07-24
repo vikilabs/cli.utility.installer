613d14e009fa0aeaf1568dde957a195a65ad5326
Vignesh Natarajan <vickyemailbox@gmail.com>
2020-07-25 01:59:58 +0200

    installer & uninstaller

---
+++ README.md
@@ -0,0 +1,30 @@
+## Utility to install and uninstall CLI utilities in Linux based systems
+
+   Scripts to install and uninstall command line utilities in Linux based systems.
+
+
+## Download Utilities
+
+  1. Clone this repo
+  2. cd src
+  3. chmod +x installer.sh uninstaller.sh
+
+## Usage
+
+#### SYNTAX - Installer
+
+	./installer.sh <utility_name>
+	
+	<utility_name> : utility to install
+
+#### SYNTAX - uninstaller
+
+  
+	./uninstaller.sh <utility_name>
+	
+	<utility_name> : utility to uninstall
+
+## Tested Linux Based Systems
+
+	1. Ubuntu 16.04
+	2. MacOS 10.15.3
---
+++ src/uninstaller.sh
@@ -0,0 +1,73 @@
+#! /bin/bash
+
+INSTALL_DIR="/usr/bin"
+UTILITY_NAME=""
+
+ STATUS_STR="[ status    ]"
+SUCCESS_STR="[ success   ]"
+FAILURE_STR="[ failure   ]"
+  ERROR_STR="[ error     ]"
+
+get_installation_directory(){
+    echo "$STATUS_STR Identifying Host System"
+    linux_system=`uname -srm | grep -i "linux"`
+    mac_system=`uname -srm | grep -i "darwin"`
+
+    if [ ! -z "$linux_system" ]; then
+        echo "$STATUS_STR LinuxOS System "
+        INSTALL_DIR="/usr/bin"
+    fi
+
+    if [ ! -z "$mac_system" ]; then
+        echo "$STATUS_STR MacOS System"
+        INSTALL_DIR="/opt/local/bin/"
+    fi
+}
+
+uninstall_package(){
+    rm $INSTALL_DIR/$UTILITY_NAME &> /dev/null
+}
+
+check_uninstall_status(){
+    if [ $1 -eq 0 ]; then
+        echo "$SUCCESS_STR Utility ( $UTILITY_NAME ) uninstall successful"
+    else
+        echo "$FAILURE_STR Utility ( $UTILITY_NAME ) uninstall failed"
+	echo "$FAILURE_STR You should have sudo or root access to uninstall this package"
+    fi
+}
+
+usage(){
+   echo ""
+   echo "-----------------------------------------------------------"
+   echo ""
+   echo "Usage:"
+   echo ""
+   echo "	./installer.sh <utility_name>"
+   echo ""
+   echo "-----------------------------------------------------------"
+   echo ""
+}
+
+is_file_exist(){
+    if [ ! -f $1 ]; then
+	echo
+	echo "$ERROR_STR package file '$1' not installed at default dir"
+	echo "$ERROR_STR Uninstall failed!"
+    	exit 1
+    fi
+}
+
+if [ $# -lt 1 ]; then
+    echo
+    echo "$ERROR_STR usage "
+    usage
+    exit 1
+fi
+
+UTILITY_NAME="$1"
+get_installation_directory
+is_file_exist $INSTALL_DIR/$UTILITY_NAME
+uninstall_package
+check_uninstall_status $?
+
